3da4d55a36bc5cb6a13cf58035291d1123f5a314
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-03-17 13:47:55 -0400

    lctx: Clarified context.Context impl in doc.go.

--- internal/lctx/doc.go
+++ internal/lctx/doc.go
@@ -1,4 +1,4 @@
-// Package lctx provides a wrapper for the net.Listener type that implements
-// the context.Context interface and provides a channel for receiving
-// newly-accepted net.Conn clients.
+// Package lctx provides a more async-friendly wrapper for the net.Listener
+// type. It implements some of the context.Context interface's methods and
+// provides a channel for receiving newly-accepted net.Conn clients.
 package lctx
